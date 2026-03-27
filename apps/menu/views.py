0fee33e79d02b81125b1f9db72115ba7f76d8044
Vlad Korneev <veenrok@veenrok.com>
2026-03-27 22:50:06 +0300

    feat: add foods list endpoint

---
+++ apps/menu/views.py
@@ -0,0 +1,30 @@
+from django.db.models import Exists, Prefetch, OuterRef
+from apps.menu.models import FoodCategory, Food
+from rest_framework.generics import ListAPIView
+
+from apps.menu.serializers import FoodListSerializer
+
+
+class FoodsListAPIView(ListAPIView):
+    serializer_class = FoodListSerializer
+
+    def get_queryset(self):
+        published_foods_qs = Food.objects.filter(is_publish=True)
+        aditional_foods_prefetch = Prefetch("additional", queryset=published_foods_qs)
+        foods_prefetch = Prefetch(
+            "foods",
+            queryset=published_foods_qs.prefetch_related(aditional_foods_prefetch),
+        )
+
+        category_has_published_foods = Exists(
+            published_foods_qs.filter(category=OuterRef("pk"))
+        )
+
+        qs = (
+            FoodCategory.objects.annotate(
+                has_published_foods=category_has_published_foods
+            )
+            .filter(has_published_foods=True)
+            .prefetch_related(foods_prefetch)
+        )
+        return qs
