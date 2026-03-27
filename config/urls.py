6f3d52898849718037960d372ebcdc3476a6aad6
Vlad Korneev <veenrok@veenrok.com>
2026-03-27 22:50:30 +0300

    feat: register foods list endpoint

--- config/urls.py
+++ config/urls.py
@@ -2,6 +2,9 @@ from debug_toolbar.toolbar import debug_toolbar_urls
 from django.contrib import admin
 from django.urls import path
 
+from apps.menu.views import FoodsListAPIView
+
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path("api/foods/", FoodsListAPIView.as_view(), name="foods-list"),
 ] + debug_toolbar_urls()
